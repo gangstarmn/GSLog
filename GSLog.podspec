ba6b60dec7974e85c4165033f1aaf026deafbe80
Gantulga <gangstarmn@gmail.com>
2016-04-29 16:13:20 +0900

    version 0.0.1

---
+++ GSLog.podspec
@@ -0,0 +1,20 @@
+ Pod::Spec.new do |s|
+  s.name         = "GSLog"
+  s.version      = "0.0.1"
+  s.summary      = "GSLog is colored log"
+  s.description  = <<-DESC
+                    Colored log using xCodeColors. easy ti use
+                   DESC
+  s.homepage     = "https://github.com/gangstarmn/GSLog"
+  s.license      = "MIT"
+  s.author             = { "Gantulga" => "gangstarmn@gmail.com" }
+  s.platform = :ios, '8.0'
+  s.source = { :git => 'https://github.com/gangstarmn/GSLog.git', :tag => "#{s.version}" }
+  
+  s.source_files = "GSLog/**/*.{h,m}"
+  
+  s.framework = 'UIKit'
+  s.requires_arc = true
+  s.dependency 'ATLog'
+
+  end
---
+++ GSLog/GSLog.h
@@ -0,0 +1,18 @@
+//
+//  GSLog.h
+//  GSLog
+//
+//  Created by Gantulga on 4/29/16.
+//  Copyright © 2016 ZTech LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <ATLog/ATLog.h>
+
+#import "GSLocalize.h"
+
+#   define ATLogError(fmt...)       ATPrettyLog(@"\033[fg255,0,0;\033[bg255,255,255; %@ \033[;",ATStringWithFormat(fmt))
+#   define ATLogWarning(fmt...)     ATPrettyLog(@"\033[fg255,255,0;\033[bg220,0,0; %@ \033[;",ATStringWithFormat(fmt))
+#   define ATLogURL(fmt...)         ATPrettyLog(@"\033[fg0,0,255;\033[bg255,255,255; %@ \033[;",ATStringWithFormat(fmt))
+#   define ATLogResponse(fmt...)    ATPrettyLog(@"\033[fg0,0,255;\033[bg255,255,255; %@ \033[;",ATStringWithFormat(fmt))
